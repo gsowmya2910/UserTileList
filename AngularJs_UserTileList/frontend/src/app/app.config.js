210bd3f0e35bc089042a379f83604c8d3e6ba13e
gsowmya2910 <gsowmya2910@gmail.com>
2017-10-08 15:46:55 -0700

    removed unwanted files

--- AngularJs_UserTileList/frontend/src/app/app.config.js
+++
@@ -1,6 +0,0 @@
-export default function appConfig($urlRouterProvider, $locationProvider) {
-    'ngInject';
-
-    $locationProvider.html5Mode(true);
-    $urlRouterProvider.otherwise('/');
-}
--- AngularJs_UserTileList/frontend/src/app/pages/index.js
+++
@@ -1,7 +0,0 @@
-import angular from 'angular';
-
-import routing from './routes';
-
-export default angular.module('app.index', [])
-    .config(routing)
-    .name;
