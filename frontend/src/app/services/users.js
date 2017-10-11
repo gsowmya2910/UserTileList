4c063f120cb30d8d2332786112871ac3a629d48d
gsowmya2910 <gsowmya2910@gmail.com>
2017-10-10 21:01:49 -0700

    updated list for chosen user

--- frontend/src/app/services/users.js
+++ frontend/src/app/services/users.js
@@ -2,6 +2,7 @@ import angular from 'angular';
 
 const serviceName = 'users';
 const entityEndpointUrl = 'https://jsonplaceholder.typicode.com/users';
+//const url='src/utils/some.json';
 
 export class UsersService {
     /* @ngInject */
@@ -9,12 +10,35 @@ export class UsersService {
         this.$http = $http;
     }
 
-    getData({ config = {} }) {
-        return this.$http
-            .get(`${entityEndpointUrl}`, config);
-    }
+     getData({ config = {} }) {
+         return this.$http
+             .get(`${entityEndpointUrl}`, config);
+     }
+
+
+    //  getData({ config = {} }) {
+    //      return this.$http
+    //          .get(`${url}`, config);
+    //  }
 }
 
 export default angular.module(`services.${serviceName}`, [])
     .service(`${serviceName}Service`, UsersService)
     .name;
+
+
+
+    // myModule.factory('Items', ['$http', function($http){
+    //     var Url   = "src/utils/some.json";
+    //     var Items = $http.get(Url).then(function(response){
+    //      return response.data;
+    //   });
+    //   return Items;
+    // }]);
+    // users.factory('userDetails', ['$http', function($http){
+    //     var Url   = "src/utils/some.json";
+    //     var userDetails = $http.get(Url).then(function(response){
+    //      return response.data;
+    //   });
+    //   return userDetails;
+    // }]);
